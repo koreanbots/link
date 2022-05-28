1ac955056b9477f93d49f21f25d392cc7d907b01
chul0721 <chul070721@naver.com>
2022-05-28 11:38:02 +0900

    feat: add custom link

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const Koa = require('koa')
+const BodyParser = require('koa-bodyparser')
 const Router = require('koa-router')
 require('dotenv').config()
 
@@ -7,8 +8,35 @@ const router = new Router()
 
 const knex = require('./db')
 
+app.use(BodyParser())
+
 router.all('/', (ctx) => {
   ctx.redirect('https://koreanbots.dev')
+})
+
+router.post('/regist', async (ctx) => {
+  const { id, redirect, category } = ctx.request.body
+  const password = ctx.request.header.authorization
+
+  if(!id || !redirect || !category) return ctx.body = { success: false, error: 'Please insert id, url, and category' }
+  if(password !== process.env.SECRET_KEY) return ctx.body = { success: false, error: 'Invalid password' }
+
+  await knex('link').insert({
+    id,
+    redirect,
+    category
+  }).then(() => {
+    ctx.body = {
+      success: true
+    }
+    console.log('success')
+  }).catch((err) => {
+    console.log(err)
+    return ctx.body = {
+      success: false,
+      error: 'Internal Server Error'
+    }
+  })
 })
 
 router.all('/:id', async (ctx) => {
@@ -20,6 +48,42 @@ router.all('/:id', async (ctx) => {
     return
   }
   else {
+    ctx.redirect(link[0].redirect)
+  }
+})
+
+router.all('/b/:id', async (ctx) => {
+  const link = await knex('link').where({ id: ctx.params.id, category: 'b' })
+  if(link.length === 0) {
+    ctx.status = 404
+    ctx.body = 'Not Found'
+    return
+  }
+   else {
+    ctx.redirect(link[0].redirect)
+  }
+})
+
+router.all('/u/:id', async (ctx) => {
+  const link = await knex('link').where({ id: ctx.params.id, category: 'u' })
+  if(link.length === 0) {
+    ctx.status = 404
+    ctx.body = 'Not Found'
+    return
+  }
+   else {
+    ctx.redirect(link[0].redirect)
+  }
+})
+
+router.all('/s/:id', async (ctx) => {
+  const link = await knex('link').where({ id: ctx.params.id, category: 's' })
+  if(link.length === 0) {
+    ctx.status = 404
+    ctx.body = 'Not Found'
+    return
+  }
+   else {
     ctx.redirect(link[0].redirect)
   }
 })
