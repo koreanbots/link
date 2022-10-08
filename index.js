3f2aaa3c767f0fa09d7dab78a8cd44eeac42ae81
Byungchul Kim <64084503+chul0721@users.noreply.github.com>
2022-10-08 22:56:12 +0900

    feat: add custom link (#4)
    * feat: add custom link
    
    * refactor: separate tables
    
    * Update .env.example

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const Koa = require('koa')
+const BodyParser = require('koa-bodyparser')
 const Router = require('koa-router')
 require('dotenv').config()
 
@@ -7,8 +8,36 @@ const router = new Router()
 
 const knex = require('./db')
 
+app.use(BodyParser())
+
+app.use(async (ctx, next) => {
+  try {
+    await next()
+  } catch (err) {
+    ctx.status = err.statusCode || err.status || 500
+    ctx.body = {
+      message: err.message
+    }
+  }
+})
+
 router.all('/', (ctx) => {
   ctx.redirect('https://koreanbots.dev')
+})
+
+router.post('/regist', async (ctx) => {
+  const { id, redirect, category } = ctx.request.body
+  const password = ctx.request.header.authorization
+
+  if(!id || !redirect || !category) return ctx.body = { success: false, error: 'Please insert id, url, and category' }
+  if(password !== process.env.SECRET_KEY) return ctx.body = { success: false, error: 'Invalid password' }
+
+  await knex(category).insert({
+    id,
+    redirect
+  }).then(
+    ctx.status = 200
+  )
 })
 
 router.all('/:id', async (ctx) => {
@@ -20,6 +49,42 @@ router.all('/:id', async (ctx) => {
     return
   }
   else {
+    ctx.redirect(link[0].redirect)
+  }
+})
+
+router.all('/b/:id', async (ctx) => {
+  const link = await knex('bot').where({ id: ctx.params.id })
+  if(link.length === 0) {
+    ctx.status = 404
+    ctx.body = 'Not Found'
+    return
+  }
+   else {
+    ctx.redirect(link[0].redirect)
+  }
+})
+
+router.all('/u/:id', async (ctx) => {
+  const link = await knex('user').where({ id: ctx.params.id })
+  if(link.length === 0) {
+    ctx.status = 404
+    ctx.body = 'Not Found'
+    return
+  }
+   else {
+    ctx.redirect(link[0].redirect)
+  }
+})
+
+router.all('/s/:id', async (ctx) => {
+  const link = await knex('server').where({ id: ctx.params.id })
+  if(link.length === 0) {
+    ctx.status = 404
+    ctx.body = 'Not Found'
+    return
+  }
+   else {
     ctx.redirect(link[0].redirect)
   }
 })
